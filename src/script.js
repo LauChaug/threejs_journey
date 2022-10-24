d529ae1d99ea0fcacef89743c60c506c24b1d80e
Chandler <104188468+LauChaug@users.noreply.github.com>
2022-10-24 17:34:46 +0800

    orbitControls (#3)

--- src/script.js
+++ src/script.js
@@ -1,7 +1,20 @@
 import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
-import { Mesh, MeshBasicMaterial } from 'three';
+import { MeshBasicMaterial } from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+
+// Cursor
+// const cursor = {
+//   x: 0,
+//   y: 0
+// }
+// window.addEventListener('mousemove', (event) => {
+//   cursor.x = event.clientX / sizes.width - 0.5
+//   cursor.y = - (event.clientY / sizes.height - 0.5)
+//   console.log(cursor.x, cursor.y);
+// })
 
 console.log(THREE);
 const scene = new THREE.Scene()
@@ -37,8 +50,8 @@ cube2.position.x = 20
 cube3.position.x = -20
 group.add(cube1, cube2, cube3)
 
-group.position.y = 10
-group.scale.y = 2
+group.position.y = 5
+// group.scale.y = 2
 // group.rotation.y = 70
 
 const sizes = {
@@ -46,29 +59,36 @@ const sizes = {
   height: 600
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1.0, 1000)
-camera.position.set(1, 15, 60)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1.0, 100)
+camera.position.set(1, 5, 20)
 // camera.position.x = 40
 // camera.position.y = 40
-camera.lookAt(0, 0, 0)
+camera.lookAt(group.position)
 scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+
+
+// OrbientControls
+// console.log(OrbitControls);
+const orbitControls = new OrbitControls(camera, canvas)
+orbitControls.enableDamping = true
 
 const axesHelper = new THREE.AxesHelper(500)
 scene.add(axesHelper)
 
 // console.log(cube.position.distanceTo(camera.position));
 
-const canvas = document.querySelector('.webgl')
-console.log(canvas);
-const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 // let time = Date.now()
 // const clock = new THREE.Clock()
 console.log(gsap);
-gsap.to(group.position, { duration: 1, delay: 1, x: 40 })
-gsap.to(group.position, { duration: 1, delay: 3, x: 0 })
+// gsap.to(group.position, { duration: 1, delay: 1, x: 40 })
+// gsap.to(group.position, { duration: 1, delay: 3, x: 0 })
 
 const tick = () => {
   // const currentTime = Date.now()
@@ -77,8 +97,14 @@ const tick = () => {
   // group.rotation.x -= 0.01
   // group.rotation.y += 0.001 * deltaTime
   // const elapsedTime = clock.getElapsedTime()
+  // update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 30
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 30
+  // camera.position.y = cursor.y * 40
+  // camera.lookAt(group.position)
   // console.log(elapsedTime);
   // group.rotation.y = Math.cos(elapsedTime * 2)
+  orbitControls.update()
   renderer.render(scene, camera)
   requestAnimationFrame(tick)
 }
